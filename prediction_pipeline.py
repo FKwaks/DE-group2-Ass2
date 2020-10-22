bbfb1348b0e3c26962f9646dd86121559cf98eb8
Kwaks <f.m.kwaks@student.tue.nl>
2020-10-22 17:48:45 +0200

    requirements toegevoegd

--- prediction_pipeline.py
+++ prediction_pipeline.py
@@ -30,6 +30,9 @@ from apache_beam.options.pipeline_options import SetupOptions
 from google.cloud import storage
 from keras.models import load_model
 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
 
 def get_csv_reader(readable_file):
     # Open a channel to read the file from GCS
