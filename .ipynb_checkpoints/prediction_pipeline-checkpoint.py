e63ca8bfb53ed33e3bb83f8565d5a5d2c493ddfd
Jasper <jasper@juistdit.nl>
2020-10-28 11:46:17 +0100

    Pipeline

--- .ipynb_checkpoints/prediction_pipeline-checkpoint.py
+++ .ipynb_checkpoints/prediction_pipeline-checkpoint.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import
-
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
 import argparse
 import csv
 import io
-import json
 import logging
 
 import apache_beam as beam
@@ -11,63 +10,46 @@ import pandas as pd
 from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
-from google.cloud import storage
-from keras.layers import Dense
-from keras.models import Sequential
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
+from keras.models import load_model
 
 
-def train_save_model(readable_file):
+def get_csv_reader(readable_file):
     # Open a channel to read the file from GCS
     gcs_file = beam.io.filesystems.FileSystems.open(readable_file)
 
-    # Read it as csv, you can also use csv.reader
-    csv_dict = csv.DictReader(io.TextIOWrapper(gcs_file))
+    # Return the csv reader
+    return csv.DictReader(io.TextIOWrapper(gcs_file))
 
-    # Create the DataFrame
-    df = pd.DataFrame(csv_dict)
-    df = df.apply(pd.to_numeric)
-    dataset = df.values
-    
-    sid_obj = SentimentIntensityAnalyzer() 
-    sentiment_dict = sid_obj.polarity_scores(sentence) 
-    
-    
-    text_out = {
-        "Sentimental Compound value:": sentiment_dict['compound']
-    }
+class MyPredictDoFn(beam.DoFn):
 
-    return json.dumps(str(text_out), sort_keys=False, indent=4)
+    def __init__(self):
+        self._model = SentimentIntensityAnalyzer()
+        self.entry = None
+        
+    def calculate_sentiment(self, entry):
+        sid_obj = SentimentIntensityAnalyzer() 
+        if (type(self.entry) != str and math.isnan(self.entry)):
+            return -55
+            opinion = sid_obj.polarity_scores(self.entry)
+        return opinion['compound']
+
+    def process(self, elements, **kwargs):
+
+        df = pd.DataFrame(elements)
+
+        df['comments'] = df['comments'].apply(calculate_sentiment)
+        df = df[df['comments'] != -55]
+        df = df.groupby('date')['comments'].mean()
+        
+        return df
 
 
 def run(argv=None, save_main_session=True):
     """Main entry point; defines and runs the wordcount pipeline."""
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--input',
-        dest='input',
-        default='gs://dataflow-samples/Data/reviews.csv',
-        help='Input file to process.')
-    parser.add_argument(
-        '--output',
-        dest='output',
-        # CHANGE 1/6: The Google Cloud Storage path is required
-        # for outputting the results.
-        default='gs://YOUR_OUTPUT_BUCKET/AND_OUTPUT_PREFIX',
-        help='Output file to write results to.')
 
-    parser.add_argument(
-        '--pid',
-        dest='pid',
-        help='project id')
-
-    parser.add_argument(
-        '--mbucket',
-        dest='mbucket',
-        help='model bucket name')
     known_args, pipeline_args = parser.parse_known_args(argv)
-    print(known_args)
-    print(pipeline_args)
+
     # We use the save_main_session option because one or more DoFn's in this
     # workflow rely on global context (e.g., a module imported at module level).
     pipeline_options = PipelineOptions(pipeline_args)
@@ -75,11 +57,18 @@ def run(argv=None, save_main_session=True):
 
     # The pipeline will be run on exiting the with block.
     with beam.Pipeline(options=pipeline_options) as p:
-        output = (p | 'Create FileName Object' >> beam.Create([known_args.input])
-                  | 'TrainAndSaveModel' >> beam.FlatMap(train_save_model, known_args.pid, known_args.mbucket)
-                  )
-        output | 'Write' >> WriteToText(known_args.output)
+        # Read the text file[pattern] into a PCollection.
+        prediction_data = (p | 'CreatePCollection' >> beam.Create(['results/test-00000-of-00001.csv'])
+                           | 'ReadCSVFle' >> beam.FlatMap(get_csv_reader))
 
+        # https://beam.apache.org/releases/pydoc/2.25.0/apache_beam.transforms.util.html#apache_beam.transforms.util.BatchElements
+        # https://beam.apache.org/documentation/transforms/python/aggregation/groupintobatches/
+        output = (prediction_data
+                  | 'batch into n batches' >> beam.BatchElements(min_batch_size=50, max_batch_size=75)
+                  | 'Predict' >> beam.ParDo(MyPredictDoFn()))
+
+        output | 'WritePredictionResults' >> WriteToText(file_path_prefix="results/predictions",
+                                                         file_name_suffix=".json")
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
